aadebda0a102ea4adf4c0a70688723f755f5314d
htueko <htooko1101@gmail.com>
2026-01-23 11:56:41 +0630

    [Enhancement] fix warnings.

--- app/src/main/java/com/htueko/tenki/feature/home/view/HomeScreen.kt
+++ app/src/main/java/com/htueko/tenki/feature/home/view/HomeScreen.kt
@@ -4,7 +4,7 @@ import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.htueko.tenki.feature.home.effect.HomeEffect
 import com.htueko.tenki.feature.home.event.HomeEvent
